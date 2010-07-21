8ef488f0d984596cb1b5e571de9611c9e5242518
Kevin English <kenglish@camberhawaii.org>
2010-07-20 16:32:25 -1000

    Added filter to the home page

--- hawaiigaragesale/views.py
+++ hawaiigaragesale/views.py
@@ -1,13 +1,17 @@
 # Create your views here.
 # Create your views here.
+import time
+from datetime import date
+today = date.today()
+
 from django.template import Context, loader
 from hawaiigaragesale.models import GarageSale
 from django.http import HttpResponse
 
 def index(request):
-  latest_garage_sales = GarageSale.objects.all().order('-sale_date')[:5]
+  upcoming_garage_sales = GarageSale.objects.all().filter("sale_date >=", date.today()).order('-sale_date')[:5]
   t = loader.get_template('hawaiigaragesale/index.html')
   c = Context({
-    'latest_garage_sales': latest_garage_sales,
+    'upcoming_garage_sales': upcoming_garage_sales,
   })
   return HttpResponse(t.render(c))
