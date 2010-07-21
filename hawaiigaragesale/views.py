143189a349cc81e8f669f6dc691bb32825c28ca5
Kevin English <kenglish@camberhawaii.org>
2010-07-21 09:27:20 -1000

    Updated git, added log module

--- hawaiigaragesale/views.py
+++ hawaiigaragesale/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 # Create your views here.
 import time
+import log
 from datetime import date
 today = date.today()
 
@@ -11,6 +12,8 @@ from django.http import HttpResponse
 def index(request):
   upcoming_garage_sales = GarageSale.objects.all().filter("sale_date >=", date.today()).order('-sale_date')[:5]
   t = loader.get_template('hawaiigaragesale/index.html')
+  log.debug("My Message") 
+  log.debug("Array len is %s " % len(upcoming_garage_sales)  ) 
   c = Context({
     'upcoming_garage_sales': upcoming_garage_sales,
   })
